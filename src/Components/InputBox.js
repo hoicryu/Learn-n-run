f582d3980a0a9d8e2f7a6e5328e3149b26bc7507
Hoic <hoicryu@gmail.com>
2021-04-12 21:16:01 +0900

    [add] button component and media query

--- src/Components/InputBox.js
+++ src/Components/InputBox.js
@@ -29,12 +29,27 @@ function InputBox({
 
 const InputWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
   align-items: center;
-  padding: 1.5rem 1.5rem 1.5rem 0;
+  padding: 1.3rem 1.3rem 1.3rem 0;
+  width: 60%;
 
+  background-color: red;
+
+  @media screen and (max-width: 780px) {
+    padding: 0.8rem 0.8rem 0.8rem 0;
+  }
+
+  @media screen and (max-width: 400px) {
+    padding: 0.6rem 0.6rem 0.6rem 0;
+  }
   p {
     width: 8em;
     font-size: 1.2em;
+    @media screen and (max-width: 780px) {
+      width: 7em;
+      font-size: 0.8em;
+    }
   }
 
   div {
@@ -44,9 +59,21 @@ const InputWrapper = styled.div`
       padding: 0.5em;
       margin-left: 2rem;
       font-size: 1.2em;
-      background-color: ffffff;
       border: 1px solid #ebebeb;
       border-radius: 10px;
+      background-color: ffffff;
+
+      @media screen and (max-width: 780px) {
+        padding: 0.3em;
+        margin-left: 1.5rem;
+        font-size: 1em;
+      }
+
+      @media screen and (max-width: 400px) {
+        padding: 0.2em;
+        margin-left: 1rem;
+        font-size: 0.8em;
+      }
     }
 
     label {
